1c4094fa6ee04f0a184540d6e9e7850215211bc0
Trần Hồng Ngọc <ngocth@vissoft.vn>
2019-06-14 14:45:12 +0700

    new project

---
+++ src/main/java/com/example/demo0000/Demo0000Application.java
@@ -0,0 +1,13 @@
+package com.example.demo0000;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo0000Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo0000Application.class, args);
+    }
+
+}
