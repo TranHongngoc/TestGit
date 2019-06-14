d7800699b04a101bc139f19953ab0fe73a02090f
Trần Hồng Ngọc <ngocth@vissoft.vn>
2019-06-14 14:56:14 +0700

    config

--- src/main/java/com/example/demo0000/Demo0000Application.java
+++ src/main/java/com/example/demo0000/Demo0000Application.java
@@ -1,13 +1,40 @@
 package com.example.demo0000;
 
+import org.ocpsoft.rewrite.servlet.RewriteFilter;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-public class Demo0000Application {
+import javax.faces.webapp.FacesServlet;
+import javax.servlet.DispatcherType;
+import java.util.EnumSet;
+
+@EnableAutoConfiguration
+@ComponentScan({"com.example.demo0000"})
+public class Demo0000Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Demo0000Application.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean() {
+        FacesServlet servlet = new FacesServlet();
+        return new ServletRegistrationBean(servlet, "*.jsf");
+    }
+
+    @Bean
+    public FilterRegistrationBean rewriteFilter() {
+        FilterRegistrationBean rwFilter = new FilterRegistrationBean(new RewriteFilter());
+        rwFilter.setDispatcherTypes(EnumSet.of(DispatcherType.FORWARD, DispatcherType.REQUEST,
+                DispatcherType.ASYNC, DispatcherType.ERROR));
+        rwFilter.addUrlPatterns("/*");
+        return rwFilter;
     }
 
 }
